522109b0bdf40213c6fe729f9d88d821e70305e2
SwastikRoy <swastik0410@gmail.com>
2025-09-24 02:37:57 +0530

    Revisiting the hero

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,14 +14,14 @@ const Navbar = () => {
         <div className='container px-4 2xl:px-20 mx-auto flex justify-between items-center'>
              <img src={assets.logo} alt="CareerCrafters logo"  />{
                 user?<div className='flex items-center gap-3'>
-                    <Link to={'/applications'}>Applied Jobs</Link>
+                    <Link to={'/applications'} className='cursor-pointer'>Applied Jobs</Link>
                     <p>|</p>
                     <p>Hi, {user.firstName+" "+user.lastName}</p>
                     <UserButton/>
                 </div>
                 :<div className='flex gap-4 max-sm:text-xs'>
-                <button className='text-gray-600'>Recruiter Login</button>
-                <button onClick={e=>openSignIn()}className='bg-blue-600 text-white px-6 py-2 rounded-full'>Login</button>
+                <button className='text-gray-600 cursor-pointer'>Recruiter Login</button>
+                <button onClick={e=>openSignIn()}className='bg-blue-600 text-white px-6 py-2 rounded-full cursor-pointer'>Login</button>
             </div>
             }
             
