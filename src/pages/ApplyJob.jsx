3079fd7c33f8ab752389e3b2ad8e3e1ee8704c07
SwastikRoy <swastik0410@gmail.com>
2025-10-09 19:02:11 +0530

    Returning to the job description

--- src/pages/ApplyJob.jsx
+++ src/pages/ApplyJob.jsx
@@ -4,6 +4,9 @@ import {AppContext} from '../context/AppContext'
 import { assets, jobsData } from '../assets/assets'
 import Loading from '../components/Loading'
 import Navbar from '../components/Navbar'
+import kConvert from 'k-convert'
+import moment from 'moment'
+import JobCard from '../components/JobCard'
 
 const ApplyJob = () => {
 
@@ -23,35 +26,60 @@ const ApplyJob = () => {
     }
     
   },[id,jobs])
-  return jobsData? (
+  return jobData? (
     <>
       <Navbar/>
-      <div>
-        <div>
-          <div>
-            <div>
-              <img src={jobData.companyId.image} alt="" />
-              <div>
-                <h1>{jobData.title}</h1>
-                <div>
-                  <span>
+      <div className='min-h-screen flex flex-col py-10 container px-4 2xl:px-20 mx-auto'>
+        <div className='bg-white text-black rounded-lg w-full'>
+          <div className='flex justify-center md:justify-between flex-wrap gap-8 px-14 py-20 mb-6 bg-sky-50 border border-sky-400 rounded-xl'>
+            <div className='flex flex-col md:flex-row items-center'> 
+              <img src={jobData.companyId.image} alt=""  className='h-24 bg-white rounded-lg p-4 mr-4 max-md:mb-4 border'/>
+              <div className='text-center md:text-left text-neutral-700'>
+                <h1 className='text-2xl sm:text-4xl font-medium'>{jobData.title}</h1>
+                <div className='flex flex-row flex-wrap max-md:justify-center gap-y-2 gap-6 items-center text-gray-600 mt-2'>
+                  <span className='flex items-center gap-1'>
                     <img src={assets.suitcase_icon} alt="" />
                     {jobData.companyId.name}
                   </span>
-                  <span>
+                  <span className='flex items-center gap-1'>
                     <img src={assets.location_icon} alt="" />
                     {jobData.location}
                   </span>
-                  <span>
+                  <span className='flex items-center gap-1'>
                     <img src={assets.person_icon} alt="" />
                     {jobData.level}
                   </span>
-                  <span>
+                  <span className='flex items-center gap-1'>
                     <img src={assets.money_icon} alt="" />
-                    CTC:{jobData.salary}
+                    CTC:{kConvert.convertTo(jobData.salary)} 
                   </span>
                 </div>
               </div>
+            </div>
+
+            <div className='flex flex-col justify-center text-end text-sm max-md:mx-auto max-md:text-center'>
+              <button className='bg-blue-600 p-2.5 px-10 text-white rounded-lg'>
+                Apply Now
+              </button>
+              <p className='mt-1 text-gray-600'>Posted {moment(jobData.date).fromNow()}</p>
+            </div>
+
+          </div>
+
+          <div className='flex flex-col lg:flex-row justify-between items-start'>
+            <div className='w-full lg:w-2/3'>
+              <h2 className='font-bold text-2xl mb-4'>
+                Job Description
+              </h2>
+              <div className='rich-text' dangerouslySetInnerHTML={{__html:jobData.description}}></div>
+              <button className='bg-blue-600 p-2.5 px-10 text-white rounded-lg mt-10'>
+                Apply Now
+              </button>
+            </div>
+            {/*Right Section*/}
+            <div>
+              <h2>More from {jobData.companyId.name}</h2>
+              {jobs.filter(job=>job._id!==jobData._id && job.companyId._id===jobData.companyId._id).filter(job=>true).slice(0,4).map((job,index)=><JobCard key={index} job={job}/>)}
             </div>
           </div>
         </div>
